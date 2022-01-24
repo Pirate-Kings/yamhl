2c1d4f51740d0fc8ad4f99250cf41acb4030cf92
whi~nyaan <whinyaan@noreply.com>
2022-01-25 03:51:23 +0800

    Add site

---
+++ site/script.js
@@ -0,0 +1,91 @@
+function createCopyButton(highlightDiv) {
+    const button = document.createElement("button");
+    button.className = "copy-code-button";
+    button.type = "button";
+    button.innerText = "Copy";
+    button.addEventListener("click", () =>
+        copyCodeToClipboard(button, highlightDiv)
+    );
+    addCopyButtonToDom(button, highlightDiv);
+}
+
+async function copyCodeToClipboard(button, highlightDiv) {
+    const codeToCopy = highlightDiv.querySelector(":last-child > pre > code")
+        .innerText;
+    try {
+        result = await navigator.permissions.query({ name: "clipboard-write" });
+        if (result.state == "granted" || result.state == "prompt") {
+            await navigator.clipboard.writeText(codeToCopy);
+        } else {
+            copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+        }
+    } catch (_) {
+        copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+    } finally {
+        codeWasCopied(button);
+    }
+}
+
+function copyCodeBlockExecCommand(codeToCopy, highlightDiv) {
+    const textArea = document.createElement("textArea");
+    textArea.contentEditable = "true";
+    textArea.readOnly = "false";
+    textArea.className = "copyable-text-area";
+    textArea.value = codeToCopy;
+    highlightDiv.insertBefore(textArea, highlightDiv.firstChild);
+    const range = document.createRange();
+    range.selectNodeContents(textArea);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+    textArea.setSelectionRange(0, 999999);
+    document.execCommand("copy");
+    highlightDiv.removeChild(textArea);
+}
+
+function codeWasCopied(button) {
+    button.blur();
+    button.innerText = "Copied!";
+    setTimeout(function () {
+        button.innerText = "Copy";
+    }, 2000);
+}
+
+function addCopyButtonToDom(button, highlightDiv) {
+    highlightDiv.insertBefore(button, highlightDiv.firstChild);
+    const wrapper = document.createElement("div");
+    wrapper.className = "highlight-wrapper";
+    highlightDiv.parentNode.insertBefore(wrapper, highlightDiv);
+    wrapper.appendChild(highlightDiv);
+}
+
+document
+    .querySelectorAll(".highlight")
+    .forEach((highlightDiv) => createCopyButton(highlightDiv));
+
+function on() {
+    document.getElementById("overlay").style.display = "grid";
+    document.getElementById("overlay").innerHTML = '<img id="ol-img"><p>Click anywhere to zoom out.</p><p>Image not zooming in enough? Create a pull request at <a href=https://github.com/MangDL/mdl>github.com/MangDL/mdl</a> and fix the issue yourself.</p>';
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function enlarge(item) {
+    on();
+    document.getElementById("overlay").style.display = "grid";
+    document.getElementById("overlay").innerHTML = `<img id="ol-img" src="${item.src}"><p>Click anywhere to zoom out.</p><p>Image not zooming in enough? Create a pull request at <a href=https://github.com/MangDL/mdl>github.com/MangDL/mdl</a> and fix the issue yourself.</p>`;
+}
+
+function addEnlarge(enlargeDiv) {
+    enlargeDiv.setAttribute("title", "Click me to enlarge!")
+}
+
+for (var i of document.querySelectorAll("ul li img")) {
+    i.setAttribute("onclick", "enlarge(this)");
+}
+
+document
+    .querySelectorAll("img")
+    .forEach((enlargeDiv) => addEnlarge(enlargeDiv));
